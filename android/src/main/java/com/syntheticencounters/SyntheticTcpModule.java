5546c12b4dd7cd6d4389567ec5d10080e30b1770
syntheticencounters <36394356+syntheticencounters@users.noreply.github.com>
2019-11-21 16:01:41 -0600

    Android
    -Android support added

--- android/src/main/java/com/syntheticencounters/SyntheticTcpModule.java
+++ android/src/main/java/com/syntheticencounters/SyntheticTcpModule.java
@@ -5,13 +5,23 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.Thread.sleep;
+
 public class SyntheticTcpModule extends ReactContextBaseJavaModule {
 
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
     private final ReactApplicationContext reactContext;
 
     public SyntheticTcpModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+
     }
 
     @Override
@@ -20,8 +30,73 @@ public class SyntheticTcpModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void connect(String host, int port, Callback callback) {
+
+        if(host == null) {
+            callback.invoke("Missing required host and/or port", null);
+            return;
+        }
+
+        try {
+            socket = new Socket(host,port);
+            System.out.println("connected");
+            callback.invoke(null, null);
+
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+            callback.invoke("Connection refused", null);
+        }
+    }
+
+    @ReactMethod
+    public void read(int timeout, Callback callback) {
+
+        if(socket == null) {
+            callback.invoke("Connection has not been setup", null);
+            return;
+        }
+
+        try {
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String line;
+            String response = "";
+
+            sleep(250);
+            while(reader.ready() && (line = reader.readLine()) != null) {
+                System.out.printf("m=readInput status=read, line=%s%n" , line);
+                response = response.concat(line);
+                response = response.concat("\r\n");
+            }
+
+            System.out.println("finished reading");
+            callback.invoke(null, response);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.toString());
+            callback.invoke("Unable to read the data received", null);
+        }
+    }
+
+    @ReactMethod
+    public void write(String string, Callback callback) {
+
+        if(socket == null) {
+            callback.invoke("Connection has not been setup", null);
+            return;
+        }
+
+        try {
+
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println(string);
+            writer.flush();
+            callback.invoke(null, null);
+
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+            callback.invoke("Unable to send the data to the receiver", null);
+        }
     }
 }
--- se-tcp.podspec
+++ se-tcp.podspec
@@ -3,24 +3,21 @@ require "json"
 package = JSON.parse(File.read(File.join(__dir__, "package.json")))
 
 Pod::Spec.new do |s|
-  s.name         = "se-tcp"
+  s.name         = "SyntheticTcp"
   s.version      = package["version"]
   s.summary      = package["description"]
   s.description  = <<-DESC
                   se-tcp
                    DESC
-  s.homepage     = "https://github.com/github_account/se-tcp"
+  s.homepage     = "https://github.com/syntheticencounters/Synthetic-Encounters-TCP"
   s.license      = "MIT"
   # s.license    = { :type => "MIT", :file => "FILE_LICENSE" }
-  s.authors      = { "Your Name" => "yourname@email.com" }
+  s.authors      = { "Mike Carpenter" => "syntheticencounters@gmail.com" }
   s.platforms    = { :ios => "9.0", :tvos => "10.0" }
-  s.source       = { :git => "https://github.com/github_account/se-tcp.git", :tag => "#{s.version}" }
+  s.source       = { :git => "https://github.com/syntheticencounters/Synthetic-Encounters-TCP.git" }
 
   s.source_files = "ios/**/*.{h,m,swift}"
   s.requires_arc = true
 
   s.dependency "React"
-	
-  # s.dependency "..."
 end
-
