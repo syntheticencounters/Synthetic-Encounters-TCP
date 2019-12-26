37a3debd1ca561d843670c8fbc9571349435553b
syntheticencounters <36394356+syntheticencounters@users.noreply.github.com>
2019-12-26 09:48:45 -0600

    Updates
    -timeouts added for read and write methods

--- ios/SyntheticTcp.m
+++ ios/SyntheticTcp.m
@@ -19,20 +19,21 @@ RCTResponseSenderBlock connectCallback = NULL;
 RCT_REMAP_METHOD(connect,
                  host:(NSString *)host
                  port:(nonnull NSNumber *)port
+                 timeout:(nonnull NSNumber *)timeout
                  callback:(RCTResponseSenderBlock)callback)
 {
-    
+
     if(host == NULL || port == NULL) {
         callback(@[@"Missing required host and/or port", [NSNull null]]);
         return;
     }
-   
+
     connectCallback = callback;
     mainQueue = dispatch_get_main_queue();
     asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:mainQueue];
-    
+
     NSError *error = nil;
-    if(![asyncSocket connectToHost:host onPort:port.shortValue viaInterface:NULL withTimeout:5 error:&error]) {
+    if(![asyncSocket connectToHost:host onPort:port.shortValue viaInterface:NULL withTimeout:[timeout doubleValue] error:&error]) {
         callback(@[@"Connection was refused by the Comm Link", [NSNull null]]);
         return;
     }
@@ -42,50 +43,51 @@ RCT_REMAP_METHOD(read,
                  timeout:(nonnull NSNumber *)timeout
                  callback:(RCTResponseSenderBlock)callback)
 {
+        /*
     if(![asyncSocket isConnected]) {
         callback(@[@"The connection between your device and the Comm Link has been lost", [NSNull null]]);
         return;
     }
-    
+         */
+
     readCallback = callback;
-    [asyncSocket readDataWithTimeout:timeout.shortValue tag:1];
+    [asyncSocket readDataWithTimeout:[timeout doubleValue] tag:1];
 }
 
 RCT_REMAP_METHOD(write,
                  string:(NSString *)string
+                 timeout:(nonnull NSNumber *)timeout
                  callback:(RCTResponseSenderBlock)callback)
 {
+        /*
     if(![asyncSocket isConnected]) {
         callback(@[@"The connection between your device and the Comm Link has been lost", [NSNull null]]);
         return;
     }
-    
+        */
+
     if(string == NULL) {
         callback(@[@"Missing required string for data", [NSNull null]]);
         return;
     }
-    
+
     writeCallback = callback;
     NSData* data = [string dataUsingEncoding:NSUTF8StringEncoding];
-    [asyncSocket writeData:data withTimeout:5 tag:2];
+    [asyncSocket writeData:data withTimeout:[timeout doubleValue] tag:2];
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
 {
     NSLog(@"sockettcp connect :%@", host);
     if(connectCallback != NULL) {
-
         connectCallback(@[[NSNull null], [NSString stringWithFormat:@"Connected to host %@", host]]);
-        connectCallback = NULL;
     }
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag
 {
     if(writeCallback != NULL) {
-
         writeCallback(@[[NSNull null], [NSNull null]]);
-        writeCallback = NULL;
     }
 }
 
@@ -94,9 +96,23 @@ RCT_REMAP_METHOD(write,
     NSString *httpResponse = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"%@", [NSString stringWithFormat:@"sockettcp response: %@", httpResponse]);
     if(readCallback != NULL) {
-
         readCallback(@[[NSNull null], httpResponse]);
-        readCallback = NULL;
+    }
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock shouldTimeoutReadWithTag:(long)tag
+elapsed:(NSTimeInterval)elapsed bytesDone:(NSUInteger)length {
+
+    if(readCallback != NULL) {
+        readCallback(@[@"The connection to the Comm Link was unsuccessful. Please restart the Comm Link and try again", [NSNull null]]);
+    }
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock shouldTimeoutWriteWithTag:(long)tag
+elapsed:(NSTimeInterval)elapsed bytesDone:(NSUInteger)length {
+
+    if(writeCallback != NULL) {
+        writeCallback(@[@"The connection to the Comm Link was unsuccessful. Please restart the Comm Link and try again", [NSNull null]]);
     }
 }
 
@@ -105,19 +121,10 @@ RCT_REMAP_METHOD(write,
     NSLog(@"sockettcp error");
     NSLog(@"%@", err.localizedDescription);
     if(connectCallback) {
-        
         if(err.code == 3) {
-            connectCallback(@[@"We were unable to establish a connection within an appropriate amount of time", [NSNull null]]);
-            connectCallback = NULL;
+            connectCallback(@[@"The connection to the Comm Link has been lost. Please restart the Comm Link and try again", [NSNull null]]);
         }
     }
-    
-    if(readCallback != NULL) {
-
-        readCallback(@[@"Unable to read the response from the Comm Link", [NSNull null]]);
-        readCallback = NULL;
-    }
-    
 }
 
 @end
